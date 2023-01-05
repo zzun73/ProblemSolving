3b38b0624e90c8c91749424eab10e94b6778d3ef
zzun <736300@naver.com>
2023-01-05 11:17:00 +0900

    Feat: 초기 프로젝트 설정

---
+++ src/baekjoon/Template.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.io.*;
+
+public class Template {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
