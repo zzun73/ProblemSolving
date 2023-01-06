ed89e65c9923ae51b4c9267c169205df79f21a87
zzun <736300@naver.com>
2023-01-06 16:45:54 +0900

    Feat: 템플릿 양식 작성

--- src/baekjoon/Template.java
+++ src/baekjoon/Template.java
@@ -1,14 +1,23 @@
 package baekjoon;
 
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Template {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
 
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            int X = Integer.parseInt(st.nextToken());
+        }
+
+        br.close();
         bw.flush();
         bw.close();
-        br.close();
     }
 }
