8b5289028bb7406eb44ee3589586366c5e5708b5
zzun <736300@naver.com>
2023-04-11 23:57:03 +0900

    Feat: 23-04-11

---
+++ src/baekjoon/silver/Num16395/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.silver.Num16395;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (i == j || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        bw.write(dp[n - 1][k - 1] + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
---
+++ src/leetcode/easy/Num0620/Solution.java
@@ -0,0 +1,11 @@
+package leetcode.easy.Num0620;
+
+class Solution {
+/*
+    # Write your MySQL query statement below
+    SELECT *
+    From Cinema
+    WHERE id % 2 = 1 AND description != 'boring'
+    ORDER BY rating DESC;
+ */
+}
