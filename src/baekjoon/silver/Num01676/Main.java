e76f73f5452195004898be3239577ce1ebf168bf
zzun <736300@naver.com>
2023-05-26 23:39:45 +0900

    Feat: 23-05-26

---
+++ src/baekjoon/silver/Num01676/Main.java
@@ -0,0 +1,22 @@
+package baekjoon.silver.Num01676;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+        while (N >= 5) {
+            answer += N / 5;
+            N /= 5;
+        }
+        bw.write(answer + "");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
