43edb1fe318d2149537d5ad6dff2fa670d9de889
zzun73 <736300@naver.com>
2023-10-05 15:03:23 +0900

    Feat: 23-10-05

---
+++ src/baekjoon/silver/Num01904/Main.java
@@ -0,0 +1,23 @@
+package baekjoon.silver.Num01904;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[1_000_001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+        bw.write(String.valueOf(dp[N]));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
