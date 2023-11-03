6dd2415292344ae4b33b752d27739bf54a398853
zzun73 <736300@naver.com>
2023-11-03 17:59:30 +0900

    Refactor: StringBuilder 추가

--- src/baekjoon/Num/Main.java
+++ src/baekjoon/Num/Main.java
@@ -8,13 +8,14 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine(), " ");
-
         for (int i = 0; i < N; i++) {
             int X = Integer.parseInt(st.nextToken());
         }
+        bw.write(sb.toString());
 
         br.close();
         bw.flush();
