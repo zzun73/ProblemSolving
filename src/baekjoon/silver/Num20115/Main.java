0fdf6242e95c52df8bab002d123e40a785ff10cf
zzun73 <736300@naver.com>
2023-11-06 20:20:32 +0900

    Feat: 23-11-06

--- src/baekjoon/silver/Num20115/Main.java
+++ src/baekjoon/silver/Num20115/Main.java
@@ -1,20 +1,27 @@
-package baekjoon;
+package baekjoon.silver.Num20115;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Template {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
+        double[] arr = new double[N];
         st = new StringTokenizer(br.readLine(), " ");
-
         for (int i = 0; i < N; i++) {
-            int X = Integer.parseInt(st.nextToken());
+            arr[i] = Double.parseDouble(st.nextToken());
         }
+        Arrays.sort(arr);
+        double answer = arr[N - 1];
+        for (int i = 0; i < N - 1; i++) {
+            answer += arr[i] / 2;
+        }
+        bw.write(String.valueOf(answer));
 
         br.close();
         bw.flush();
