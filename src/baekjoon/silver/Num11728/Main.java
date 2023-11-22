6d820bd3f3eed13beae49d029c739d75e053182d
zzun73 <736300@naver.com>
2023-11-22 23:49:12 +0900

    Feat: 23-11-22

---
+++ src/baekjoon/silver/Num11728/Main.java
@@ -0,0 +1,49 @@
+package baekjoon.silver.Num11728;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
+        int[] B = new int[M];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < M; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+        int i = 0, j = 0;
+        while (i < N && j < M) {
+            if (A[i] < B[j]) {
+                sb.append(A[i++]).append(" ");
+            } else {
+                sb.append(B[j++]).append(" ");
+            }
+        }
+
+        while (i < N) {
+            sb.append(A[i++]).append(" ");
+        }
+        while (j < M) {
+            sb.append(B[j++]).append(" ");
+        }
+        bw.write(sb.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
