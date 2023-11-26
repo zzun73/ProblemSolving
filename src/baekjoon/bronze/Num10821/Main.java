d32593dc4ccbc893360f6469bde870621a9ccd50
zzun73 <736300@naver.com>
2023-11-26 23:08:28 +0900

    Feat: 23-11-26

---
+++ src/baekjoon/bronze/Num10821/Main.java
@@ -0,0 +1,17 @@
+package baekjoon.bronze.Num10821;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] str = br.readLine().split(",");
+
+        bw.write(String.valueOf(str.length));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
