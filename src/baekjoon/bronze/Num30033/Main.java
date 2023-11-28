b58c7ddb7d58a4a398e4ddf0f940069a3680d070
zzun73 <736300@naver.com>
2023-11-28 23:27:45 +0900

    Feat: 23-11-28

---
+++ src/baekjoon/bronze/Num30033/Main.java
@@ -0,0 +1,32 @@
+package baekjoon.bronze.Num30033;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int answer = 0;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            if (arr[i] <= Integer.parseInt(st.nextToken())) {
+                answer++;
+            }
+        }
+        bw.write(String.valueOf(answer));
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
