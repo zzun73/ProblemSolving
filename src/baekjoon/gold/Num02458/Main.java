a2d8cc03a4153313243ba578118356a2b8955364
zzun73 <736300@naver.com>
2024-02-23 23:57:42 +0900

    Feat: 24-02-23

--- src/baekjoon/gold/Num02458/Main.java
+++ src/baekjoon/gold/Num02458/Main.java
@@ -1,10 +1,12 @@
 package baekjoon.gold.Num02458;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    private static final int INF = 1000000;
+    private static final int INF = 100000000;
+    static int N, M, a, b, count, answer;
+    static int[][] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,48 +14,43 @@ public class Main {
         StringTokenizer st;
 
         st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[][] arr = new int[N + 1][N + 1];
-
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N; j++) {
-                arr[i][j] = INF;
-            }
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][N];
+        for (int[] row : arr) {
+            Arrays.fill(row, INF);
         }
 
-        for (int i = 0; i < M; i++) {
+        while (M-- > 0) {
             st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            a = Integer.parseInt(st.nextToken()) - 1;
+            b = Integer.parseInt(st.nextToken()) - 1;
             arr[a][b] = 1;
         }
 
-        for (int k = 1; k <= N; k++) {
-            for (int i = 1; i <= N; i++) {
-                for (int j = 1; j <= N; j++) {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
                     arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
                 }
             }
         }
 
-        int answer = 0;
-        for (int i = 1; i <= N; i++) {
-            int count = 0;
-            for (int j = 1; j <= N; j++) {
+        for (int i = 0; i < N; i++) {
+            count = 0;
+            for (int j = 0; j < N; j++) {
                 if (arr[i][j] != INF || arr[j][i] != INF) {
                     count++;
                 }
             }
+
             if (count == N - 1) {
                 answer++;
             }
         }
-        bw.write(answer + "");
 
+        bw.write(String.valueOf(answer));
         br.close();
-        bw.flush();
         bw.close();
     }
 }
