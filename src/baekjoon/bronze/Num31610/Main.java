c2e64e198810c66ea81a39dda082eccd80449b3a
zzun73 <736300@naver.com>
2024-04-11 22:54:43 +0900

    Feat: 24-04-11

---
+++ src/baekjoon/bronze/Num31610/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.bronze.Num31610;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        bw.write(String.valueOf(A * B + C));
+
+        br.close();
+        bw.close();
+    }
+}
