3a2fb6956358ede4312bfa06bcae9bc95e3832a9
zzun73 <736300@naver.com>
2024-07-11 23:18:35 +0900

    Feat: 24-07-11

---
+++ src/baekjoon/silver/Num15724/Main.java
@@ -0,0 +1,44 @@
+package baekjoon.silver.Num15724;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[N + 1][M + 1];
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= M; j++) {
+                int input = Integer.parseInt(st.nextToken());
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + input - dp[i - 1][j - 1];
+            }
+        }
+
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            sb.append(dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]).append("\n");
+        }
+
+        bw.write(sb.toString());
+
+        bw.close();
+        br.close();
+    }
+}
