296fc50835bacd33c80a9568b7bd3ccc41f9dd45
남혁준 <736300@naver.com>
2024-12-31 21:51:20 +0900

    Feat: 24-12-31

---
+++ src/baekjoon/bronze/Num04589/Main.java
@@ -0,0 +1,34 @@
+package baekjoon.bronze.Num04589;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        sb.append("Gnomes:").append("\n");
+        while (N-- > 0) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a >= b && b >= c || a <= b && b <= c) {
+                sb.append("Ordered");
+            } else {
+                sb.append("Unordered");
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
