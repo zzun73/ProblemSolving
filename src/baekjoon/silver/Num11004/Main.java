16ba75ca0ddf78c161da85b779c0773860e208b5
zzun73 <736300@naver.com>
2025-02-04 21:48:51 +0900

    Feat: 25-02-04

---
+++ src/baekjoon/silver/Num11004/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.silver.Num11004;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        List<Integer> list = new ArrayList<>();
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+        Collections.sort(list);
+        bw.write(String.valueOf(list.get(K-1)));
+
+        br.close();
+        bw.close();
+    }
+}
