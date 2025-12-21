2baac1041145ad189f3dd8bf99722e6be83d9879
zzun73 <736300@naver.com>
2025-12-21 21:26:03 +0900

    Feat: 25-12-21

---
+++ src/baekjoon/bronze/Num33701/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.bronze.Num33701;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("nkugwan");
+
+        bw.close();
+    }
+}
