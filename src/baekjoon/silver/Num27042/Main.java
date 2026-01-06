9a95b9cbcf67b56aebd3839a9b247dcdf54b7ada
zzun73 <736300@naver.com>
2026-01-06 23:58:00 +0900

    Feat: 26-01-06

---
+++ src/baekjoon/silver/Num27042/Main.java
@@ -0,0 +1,28 @@
+package baekjoon.silver.Num27042;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            deque.addLast(i);
+        }
+
+        while (deque.size() > 1) {
+            deque.addLast(deque.pollFirst());
+            deque.pollFirst();
+        }
+
+        bw.write(String.valueOf(deque.poll()));
+
+        br.close();
+        bw.close();
+    }
+}
