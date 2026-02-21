b8e689c9844169b3a3e8af15345d542492f52f3d
zzun73 <736300@naver.com>
2026-02-21 18:13:57 +0900

    Feat: 26-02-21

---
+++ src/baekjoon/bronze/Num02789/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.bronze.Num02789;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(br.readLine().replaceAll("[CAMBRIDGE]", ""));
+
+        br.close();
+        bw.close();
+    }
+}
