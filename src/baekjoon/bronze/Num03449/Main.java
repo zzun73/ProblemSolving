fa5fde3fe8d6687a6a1ce54de3f65e0876b427c1
zzun73 <736300@naver.com>
2026-03-13 14:21:52 +0900

    Feat: 26-03-13

--- src/baekjoon/bronze/Num03449/Main.java
+++ src/baekjoon/bronze/Num03449/Main.java
@@ -3,22 +3,25 @@ package baekjoon.bronze.Num03449;
 import java.io.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        while (true) {
-            String str = br.readLine();
-            if (str.equals("***")) {
-                break;
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            char[] input1 = br.readLine().toCharArray();
+            char[] input2 = br.readLine().toCharArray();
+            int answer = 0;
+
+            for (int i = 0; i < input1.length; i++) {
+                if (input1[i] != input2[i]) {
+                    answer++;
+                }
             }
-            StringBuilder cur = new StringBuilder(str);
 
-            sb.append(cur.reverse()).append("\n");
+            sb.append("Hamming distance is ").append(answer).append(".").append("\n");
         }
-
         bw.write(sb.toString());
 
         br.close();
